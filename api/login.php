0681fa502e8e4088c69e791870c90693057b1bc2
ricardoszanata <ricardoszanata@gmail.com>
2026-03-10 09:06:47 -0300

    Criação da consulta com parâametros e da API de login

---
+++ api/login.php
@@ -0,0 +1,16 @@
+<?php
+require '../app/conexao.php';
+$pdo = Conexao::conectar();
+$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+//$json = $_GET['jsn'];
+$json = filter_input(INPUT_GET,'jsn');
+$data2 = json_decode($json,true);
+$usuario = $data2['usuario'];
+$senha = $data2['senha'];
+$sql = "select * from usuarios where usulogin = '$usuario' and ususenha = MD5($senha);";
+$prp = $pdo->prepare($sql);
+$prp->execute();
+$data = $prp->fetchall(PDO::FETCH_ASSOC);
+echo json_encode($data);
+//{"nome":"valor"}
+//http://localhost/Projetos_ETEC_PWEB-III_Div1/api/login.php?jsn={"usuario":"xandao","senha":"123456"}
